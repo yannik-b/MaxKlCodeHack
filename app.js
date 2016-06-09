ea31edfa45f4f40b004371b61bca4beed723a07b
Yannik Bürkle <mail@yannik-buerkle.de>
2016-06-09 23:29:57 +0200

    verbesserungen

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var request = require('request'),
     fs = require('fs');
 
 var file,
+	begin,
     found = 0,
     counter = 0,
+	failed = 0,
 	connections = 20;
 
 // Code Style
@@ -19,8 +21,11 @@ function doCode() {
     while (code.length < cLength) { code = "0" + code; }
 
     if (counter % 40 === 0) {
+		var nowT = new Date().valueOf()
+		var timeNeeded = (nowT - begin) / 1000;
         var percentage = counter / Math.pow(cChars.length, cLength) * 100;
-        console.log("state: " + percentage + "% | checked: " + counter + " | left: " + (Math.pow(cChars.length, cLength) - counter) + " | found: " + found);
+		var left = (Math.pow(cChars.length, cLength) - counter);
+        console.log("state: " + percentage + "% | checked: " + counter + " | left: " + left + " | time-needed: " + timeNeeded + "s | ETA: " + (counter/timeNeeded*(Math.pow(cChars.length, cLength)-counter)) + "s | " + (counter/timeNeeded) + " req/s | failed res: " + failed + " | found: " + found);
     }
 
     request({
@@ -34,6 +39,7 @@ function doCode() {
         if (err) {
 			console.dir("invalid response: " + err);
 			counter--;
+			failed++;
 		}
 		else {
 	        var found = JSON.parse(body).found;
@@ -53,8 +59,9 @@ if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
 	counter = process.argv[3];
 	connections = process.argv[2] - 1;
 	var now = new Date();
-	var date = now.getYear() + now.getMonth() + now.getDate() + "-" + now.getHours() + now.getMinutes() + now.getSeconds();
-	var file = fs.createWriteStream("founds-" + date + "-" + process.pid + ".txt", {
+	begin = now.valueOf();
+	var date = now.getYear()+1900 + "-" + now.getMonth() + "-" + now.getDate() + "_" + now.getHours() + "-" + now.getMinutes() + "-" + now.getSeconds();
+	var file = fs.createWriteStream("founds_" + date + "_" + process.pid + ".txt", {
 	    flags: 'a',
 	    defaultEncoding: 'utf8',
 	    fd: null,
